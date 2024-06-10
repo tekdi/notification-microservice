13f064beaa7db946639a2887f0e7a8205ec3f5e9
poojakarma <pooja.karma@tekditechnologies.com>
2024-06-10 15:47:39 +0530

    Change name of entities and col name

--- src/modules/notification_events/notification_events.service.ts
+++ src/modules/notification_events/notification_events.service.ts
@@ -1,12 +1,12 @@
 import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { NotificationTemplates } from './entity/notificationTemplate.entity';
+import { NotificationActions } from './entity/notificationActions.entity';
 import { SearchFilterDto } from './dto/searchTemplateType.dto';
 import APIResponse from 'src/common/utils/response';
 import { Response } from 'express';
 import { CreateEventDto } from './dto/createTemplate.dto';
-import { NotificationTemplateConfig } from './entity/notificationTemplateConfig.entity';
+import { NotificationActionTemplates } from './entity/notificationActionTemplates.entity';
 import { UpdateEventDto } from './dto/updateEventTemplate.dto';
 import { LoggerService } from 'src/common/logger/logger.service';
 import { APIID } from 'src/common/utils/api-id.config';
@@ -14,10 +14,10 @@ import { APIID } from 'src/common/utils/api-id.config';
 export class NotificationEventsService {
 
     constructor(
-        @InjectRepository(NotificationTemplates)
-        private notificationTemplatesRepository: Repository<NotificationTemplates>,
-        @InjectRepository(NotificationTemplateConfig)
-        private notificationTemplateConfigRepository: Repository<NotificationTemplateConfig>,
+        @InjectRepository(NotificationActions)
+        private notificationTemplatesRepository: Repository<NotificationActions>,
+        @InjectRepository(NotificationActionTemplates)
+        private notificationTemplateConfigRepository: Repository<NotificationActionTemplates>,
         private readonly logger: LoggerService
     ) { }
 
@@ -36,7 +36,7 @@ export class NotificationEventsService {
                 );
                 throw new BadRequestException('Template Already exist');
             }
-            const notificationTemplate = new NotificationTemplates();
+            const notificationTemplate = new NotificationActions();
             notificationTemplate.title = data.title;
             notificationTemplate.key = data.key;
             notificationTemplate.status = data.status;
@@ -46,14 +46,14 @@ export class NotificationEventsService {
             const notificationTemplateResult = await this.notificationTemplatesRepository.save(notificationTemplate);
             // create config details
             const createConfig = async (type: string, configData: any) => {
-                const templateConfig = new NotificationTemplateConfig();
+                const templateConfig = new NotificationActionTemplates();
                 Object.assign(templateConfig, {
                     subject: configData.subject,
                     body: configData.body,
                     status: data.status,
                     type: type,
                     language: 'en',
-                    template_id: notificationTemplateResult.id,
+                    actionId: notificationTemplateResult.actionId,
                     createdBy: userId,
                 });
                 return await this.notificationTemplateConfigRepository.save(templateConfig);
@@ -70,7 +70,7 @@ export class NotificationEventsService {
             if (data.sms && Object.keys(data.sms).length > 0) {
                 await createConfig('sms', data.sms);
             }
-            return APIResponse.success(response, apiId, { id: notificationTemplateResult.id }, HttpStatus.CREATED, "Created");
+            return APIResponse.success(response, apiId, { id: notificationTemplateResult.actionId }, HttpStatus.CREATED, "Created");
         } catch (e) {
             this.logger.error(
                 `/Create Template for Notification`,
@@ -92,7 +92,7 @@ export class NotificationEventsService {
         const apiId = APIID.TEMPLATE_UPDATE;
         try {
             const existingTemplate = await this.notificationTemplatesRepository.findOne({
-                where: { id: id },
+                where: { actionId: id },
             });
             if (!existingTemplate) {
                 this.logger.log(
@@ -107,7 +107,7 @@ export class NotificationEventsService {
             const createConfig = async (type: string, configData?: any) => {
                 if (configData && Object.keys(configData).length > 0) {
                     let existingConfig = await this.notificationTemplateConfigRepository.findOne({
-                        where: { template_id: id, type: type },
+                        where: { actionId: id, type: type },
                     });
                     if (existingConfig) {
                         Object.assign(existingConfig, configData);
@@ -116,7 +116,7 @@ export class NotificationEventsService {
                     }
                     else {
                         existingConfig = this.notificationTemplateConfigRepository.create({
-                            template_id: id,
+                            actionId: id,
                             type: type,
                             subject: configData.subject,
                             body: configData.body,
@@ -143,7 +143,7 @@ export class NotificationEventsService {
 
             if (updateEventDto.status) {
                 let existingConfig = await this.notificationTemplateConfigRepository.find({
-                    where: { template_id: id },
+                    where: { actionId: id },
                 });
                 existingConfig.forEach(async (config) => {
                     if (updateEventDto.status) {
@@ -205,27 +205,27 @@ export class NotificationEventsService {
         }
     }
 
-    async deleteTemplate(id: number, response: Response) {
+    async deleteTemplate(actionId: number, response: Response) {
         const apiId = APIID.TEMPLATE_DELETE
         try {
-            const templateId = await this.notificationTemplatesRepository.find({ where: { id } });
+            const templateId = await this.notificationTemplatesRepository.find({ where: { actionId } });
             if (!templateId) {
                 return APIResponse.error(
                     response,
                     apiId,
-                    `No event id found: ${id}`,
+                    `No event id found: ${actionId}`,
                     'records not found.',
                     HttpStatus.NOT_FOUND
                 )
             }
-            const deleteTemplate = await this.notificationTemplatesRepository.delete({ id });
+            const deleteTemplate = await this.notificationTemplatesRepository.delete({ actionId });
             if (deleteTemplate.affected !== 1) {
                 throw new BadRequestException('Template not deleted');
             }
             return APIResponse.success(
                 response,
                 apiId,
-                `Template with ID ${id} deleted successfully.`,
+                `Template with ID ${actionId} deleted successfully.`,
                 HttpStatus.OK,
                 'deleted successfully',
             )
