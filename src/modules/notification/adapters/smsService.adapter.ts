f113e98eb43f75420a02f859ad9e1ed3dd998366
Shubham <shubham.jha@tekditechnologies.com>
2025-04-04 18:41:36 +0530

    Added Adapter of AWS and twillio

--- src/modules/notification/adapters/smsService.adapter.ts
+++ src/modules/notification/adapters/smsService.adapter.ts
@@ -11,11 +11,21 @@ import { NotificationService } from "../notification.service";
 import { LoggerUtil } from "src/common/logger/LoggerUtil";
 import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "src/common/utils/constant.util";
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import { SMS_PROVIDER } from "src/common/utils/constant.util";
+
 
 @Injectable()
 export class SmsAdapter implements NotificationServiceInterface {
+    private readonly smsProvider: string;
+    
+    // For twillio
+    private readonly twilioAccountSid: string;
+    private readonly twilioAuthToken: string;
+    private readonly twilioSmsFrom: string;
+    
+    // For AWS SNS 
     private readonly snsClient: SNSClient;
-    private readonly smsSenderId;
+    private readonly awsSmsSenderId: string;
 
     constructor(
         @Inject(forwardRef(() => NotificationService)) private readonly notificationServices: NotificationService,
@@ -25,14 +35,29 @@ export class SmsAdapter implements NotificationServiceInterface {
         @InjectRepository(NotificationActionTemplates)
         private notificationTemplateConfigRepository: Repository<NotificationActionTemplates>,
     ) {
-        this.snsClient = new SNSClient({
-            region: this.configService.get('AWS_REGION'),
-            credentials: {
-                accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
-            },
-        });
-        this.smsSenderId = this.configService.get('AWS_SNS_SENDER_ID');
+        // Determine SMS provider from environment
+        this.smsProvider = this.configService.get('SMS_PROVIDER', SMS_PROVIDER.AWS_SNS); // Default to AWS if not specified
+        
+        // Initialize provider-specific configurations
+        if (this.smsProvider === SMS_PROVIDER.TWILIO) {
+            this.twilioAccountSid = this.configService.get('TWILIO_ACCOUNT_SID');
+            this.twilioAuthToken = this.configService.get('TWILIO_AUTH_TOKEN');
+            this.twilioSmsFrom = this.configService.get('SMS_FROM');
+            LoggerUtil.log(`SMS Provider configured: Twilio`);
+        } else if (this.smsProvider === SMS_PROVIDER.AWS_SNS) {
+            this.snsClient = new SNSClient({
+                region: this.configService.get('AWS_REGION'),
+                credentials: {
+                    accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+                },
+            });
+            this.awsSmsSenderId = this.configService.get('AWS_SNS_SENDER_ID');
+            LoggerUtil.log(`SMS Provider configured: AWS SNS`);
+        } else {
+            LoggerUtil.error(`Invalid SMS provider configured: ${this.smsProvider}`);
+            throw new Error(`Invalid SMS provider: ${this.smsProvider}. Supported providers are TWILIO and AWS_SNS.`);
+        }
     }
 
     async sendNotification(notificationDataArray) {
@@ -43,6 +68,7 @@ export class SmsAdapter implements NotificationServiceInterface {
                 if (!this.isValidMobileNumber(recipient)) {
                     throw new BadRequestException(ERROR_MESSAGES.INVALID_MOBILE_NUMBER);
                 }
+                
                 const smsNotificationDto = {
                     body: notificationData.body,
                     recipient: recipient,
@@ -50,7 +76,9 @@ export class SmsAdapter implements NotificationServiceInterface {
                     key: notificationData.key,
                     subject: notificationData.subject,
                 };
+                
                 const result = await this.send(smsNotificationDto);
+                
                 results.push({
                     recipient: recipient,
                     status: 200,
@@ -92,23 +120,16 @@ export class SmsAdapter implements NotificationServiceInterface {
             notificationData.body, 
             notificationData.recipient
         );
+        
         try {
-            const command = new PublishCommand({
-                Message: notificationData.body,
-                PhoneNumber: `+91${notificationData.recipient}`,
-                MessageAttributes: {
-                    'AWS.SNS.SMS.SenderID': {
-                        DataType: 'String',
-                        StringValue: this.smsSenderId,
-                    },
-                    'AWS.SNS.SMS.SMSType': {
-                        DataType: 'String',
-                        StringValue: 'Transactional',
-                    },
-                },
-            });
+            let response;
             
-            const response = await this.snsClient.send(command);
+            if (this.smsProvider === SMS_PROVIDER.TWILIO) {
+                response = await this.sendViaTwilio(notificationData);
+            } else if (this.smsProvider === SMS_PROVIDER.AWS_SNS) {
+                response = await this.sendViaAwsSns(notificationData);
+            }
+            
             LoggerUtil.log(SUCCESS_MESSAGES.SMS_NOTIFICATION_SEND_SUCCESSFULLY);
             notificationLogs.status = true;
             await this.notificationServices.saveNotificationLogs(notificationLogs);
@@ -120,5 +141,37 @@ export class SmsAdapter implements NotificationServiceInterface {
             await this.notificationServices.saveNotificationLogs(notificationLogs);
             throw error;
         }
+    }
+    
+    private async sendViaTwilio(notificationData) {
+        const twilio = require('twilio');
+        const client = twilio(this.twilioAccountSid, this.twilioAuthToken);
+        
+        const message = await client.messages.create({
+            from: `${this.twilioSmsFrom}`,
+            to: `+91${notificationData.recipient}`,
+            body: notificationData.body,
+        });
+        
+        return message;
+    }
+    
+    private async sendViaAwsSns(notificationData) {
+        const command = new PublishCommand({
+            Message: notificationData.body,
+            PhoneNumber: `+91${notificationData.recipient}`,
+            MessageAttributes: {
+                'AWS.SNS.SMS.SenderID': {
+                    DataType: 'String',
+                    StringValue: this.awsSmsSenderId,
+                },
+                'AWS.SNS.SMS.SMSType': {
+                    DataType: 'String',
+                    StringValue: 'Transactional',
+                },
+            },
+        });
+        
+        return await this.snsClient.send(command);
     }
 }
