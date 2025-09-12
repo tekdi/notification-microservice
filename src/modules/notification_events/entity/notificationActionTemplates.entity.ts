5b067f5cc6de771f782b321a3e46c117bd460631
Tushar <tushar.mahajan@tekditechnologies.com>
2025-09-12 16:18:55 +0530

    Update API created

--- src/modules/notification_events/entity/notificationActionTemplates.entity.ts
+++ src/modules/notification_events/entity/notificationActionTemplates.entity.ts
@@ -42,12 +42,6 @@ export class NotificationActionTemplates {
   @Column({ nullable: true })
   link: string;
 
-  @Column({ nullable: true })
-  emailFromName: string;
-
-  @Column({ nullable: true })
-  emailFrom: string;
-
   @Column({ type: 'uuid', nullable: true })
   updatedBy: string;
 
